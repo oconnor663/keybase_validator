74e6d97512863caa4a89763246fbb087828733b6
Jack O'Connor <oconnor663@gmail.com>
2019-05-13 13:20:52 -0400

    add a JSON parsing benchmark

--- verify_example/benches/bench.rs
+++ verify_example/benches/bench.rs
@@ -14,3 +14,11 @@ fn bench_verify_merkle_root(b: &mut Bencher) {
     let key = verify_example::load_key(verify_example::ROOT_KEY.as_bytes()).unwrap();
     b.iter(|| verify_example::verify(verify_example::ROOT_5360668_PGP.as_bytes(), &key).unwrap());
 }
+
+#[bench]
+fn bench_parse_root_json(b: &mut Bencher) {
+    b.iter(|| {
+        let v: serde_json::Value = serde_json::from_str(verify_example::ROOT_5360668_JSON).unwrap();
+        assert!(v.get("body").is_some());
+    });
+}
