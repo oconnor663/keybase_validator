78ae26daf3f18156dc5b86dc5bd485ff2aa3a29f
Jack O'Connor <oconnor663@gmail.com>
2019-05-13 15:45:55 -0400

    add and benchmark nacl code

--- verify_example/benches/bench.rs
+++ verify_example/benches/bench.rs
@@ -5,14 +5,27 @@ extern crate test;
 use test::Bencher;
 
 #[bench]
-fn bench_load_key(b: &mut Bencher) {
-    b.iter(|| verify_example::load_key(verify_example::ROOT_KEY.as_bytes()).unwrap());
+fn bench_pgp_load_key(b: &mut Bencher) {
+    b.iter(|| verify_example::load_pgp_key(verify_example::ROOT_PGP_KEY.as_bytes()).unwrap());
 }
 
 #[bench]
-fn bench_verify_merkle_root(b: &mut Bencher) {
-    let key = verify_example::load_key(verify_example::ROOT_KEY.as_bytes()).unwrap();
-    b.iter(|| verify_example::verify(verify_example::ROOT_5360668_PGP.as_bytes(), &key).unwrap());
+fn bench_pgp_verify_merkle_root(b: &mut Bencher) {
+    let key = verify_example::load_pgp_key(verify_example::ROOT_PGP_KEY.as_bytes()).unwrap();
+    b.iter(|| {
+        verify_example::verify_pgp(verify_example::ROOT_5360668_PGP.as_bytes(), &key).unwrap()
+    });
+}
+
+#[bench]
+fn bench_nacl_load_key(b: &mut Bencher) {
+    b.iter(|| verify_example::load_nacl_key(verify_example::ROOT_NACL_KEY).unwrap());
+}
+
+#[bench]
+fn bench_nacl_verify_merkle_root(b: &mut Bencher) {
+    let key = verify_example::load_nacl_key(verify_example::ROOT_NACL_KEY).unwrap();
+    b.iter(|| verify_example::verify_kbsig(verify_example::ROOT_5360668_KBSIG, &key).unwrap());
 }
 
 #[bench]
--- verify_example/src/lib.rs
+++ verify_example/src/lib.rs
@@ -1,28 +1,33 @@
 //! This code is mostly copied from:
 //! https://docs.sequoia-pgp.org/sequoia_guide/chapter_01/index.html
 
+use failure::{err_msg, Error};
 use sequoia_openpgp::parse::stream::{
     MessageLayer, MessageStructure, VerificationHelper, VerificationResult, Verifier,
 };
 use sequoia_openpgp::parse::{PacketParser, Parse};
 use sequoia_openpgp::{KeyID, TPK};
+use serde::Deserialize;
 use std::io;
 
-pub static JACK_KEY: &str = include_str!("jack_key.pgp");
-pub static ROOT_KEY: &str = include_str!("root_key.pgp");
+pub static JACK_PGP_KEY: &str = include_str!("jack_key.pgp");
+pub static ROOT_PGP_KEY: &str = include_str!("root_key.pgp");
 pub static ROOT_5360668_PGP: &str = include_str!("root_5360668.pgp");
+pub static ROOT_5360668_KBSIG: &str = include_str!("root_5360668.kbsig");
 pub static ROOT_5360668_JSON: &str = include_str!("root_5360668.json");
+pub static ROOT_NACL_KEY: &str =
+    "01209ec31411b9b287f62630c2486005af27548ba62a59bbc802e656b888991a20230a";
 
-const BAD_SIG: &str = "Bad signature";
-const MISSING_KEY: &str = "Missing key to verify signature";
+const BAD_SIG: &str = "signature verification failed";
+const MISSING_KEY: &str = "missing PGP key for signature";
 
-pub fn load_key(key: &[u8]) -> sequoia_openpgp::Result<TPK> {
+pub fn load_pgp_key(key: &[u8]) -> Result<TPK, Error> {
     let parser_result = PacketParser::from_bytes(key)?;
     TPK::from_packet_parser(parser_result)
 }
 
 /// Verifies the given message.
-pub fn verify(signed_message: &[u8], sender: &TPK) -> sequoia_openpgp::Result<Vec<u8>> {
+pub fn verify_pgp(signed_message: &[u8], sender: &TPK) -> Result<Vec<u8>, Error> {
     // Make a helper that that feeds the sender's public key to the
     // verifier.
     let helper = Helper { tpk: sender };
@@ -41,12 +46,12 @@ struct Helper<'a> {
 }
 
 impl<'a> VerificationHelper for Helper<'a> {
-    fn get_public_keys(&mut self, _ids: &[KeyID]) -> sequoia_openpgp::Result<Vec<TPK>> {
+    fn get_public_keys(&mut self, _ids: &[KeyID]) -> Result<Vec<TPK>, Error> {
         // Return public keys for signature verification here.
         Ok(vec![self.tpk.clone()])
     }
 
-    fn check(&mut self, structure: &MessageStructure) -> sequoia_openpgp::Result<()> {
+    fn check(&mut self, structure: &MessageStructure) -> Result<(), Error> {
         // In this function, we implement our signature verification
         // policy.
 
@@ -62,24 +67,68 @@ impl<'a> VerificationHelper for Helper<'a> {
                     match results.get(0) {
                         Some(VerificationResult::GoodChecksum(..)) => good = true,
                         Some(VerificationResult::MissingKey(_)) => {
-                            return Err(failure::err_msg(MISSING_KEY))
+                            return Err(err_msg(MISSING_KEY))
                         }
-                        Some(VerificationResult::BadChecksum(_)) => {
-                            return Err(failure::err_msg(BAD_SIG))
-                        }
-                        None => return Err(failure::err_msg("No signature")),
+                        Some(VerificationResult::BadChecksum(_)) => return Err(err_msg(BAD_SIG)),
+                        None => return Err(err_msg("No signature")),
                     }
                 }
-                _ => return Err(failure::err_msg("Unexpected message structure")),
+                _ => return Err(err_msg("Unexpected message structure")),
             }
         }
 
         if good {
             Ok(()) // Good signature.
         } else {
-            Err(failure::err_msg("Signature verification failed"))
+            Err(err_msg("Signature verification failed"))
         }
     }
+}
+
+pub fn load_nacl_key(kid: &str) -> Result<sodiumoxide::crypto::sign::PublicKey, Error> {
+    let bytes = hex::decode(&kid)?;
+    // Strip the prefix and suffix.
+    let type_bytes = &bytes[..2];
+    let suffix_bytes = &bytes[bytes.len() - 1..];
+    let key_bytes = &bytes[2..bytes.len() - 1];
+    if type_bytes != &[0x01, 0x20] {
+        return Err(err_msg(format!("wrong key type: {:?}", type_bytes)));
+    }
+    if suffix_bytes != &[0x0a] {
+        return Err(err_msg(format!("wrong key suffix: {:?}", suffix_bytes)));
+    }
+    sodiumoxide::crypto::sign::PublicKey::from_slice(key_bytes).ok_or(err_msg("bad key length"))
+}
+
+#[derive(Debug, Deserialize)]
+struct KBSig {
+    body: KBSigBody,
+}
+
+#[derive(Debug, Deserialize)]
+struct KBSigBody {
+    // Without these bytes annotations, serde will assume Vec<u8> is an array
+    // of its, rather than the msgpack bytes type.
+    #[serde(with = "serde_bytes")]
+    sig: Vec<u8>,
+    #[serde(with = "serde_bytes")]
+    payload: Vec<u8>,
+}
+
+pub fn verify_kbsig(
+    signed_message: &str,
+    key: &sodiumoxide::crypto::sign::PublicKey,
+) -> Result<Vec<u8>, Error> {
+    let bytes = base64::decode(signed_message)?;
+    // let v: rmpv::Value = rmpv::decode::read_value(&mut &*bytes).unwrap();
+    // dbg!(v);
+    let obj: KBSig = rmp_serde::decode::from_slice(&bytes)?;
+    let sig = sodiumoxide::crypto::sign::Signature::from_slice(&obj.body.sig)
+        .ok_or(err_msg("bad sig length"))?;
+    if !sodiumoxide::crypto::sign::verify_detached(&sig, &obj.body.payload, key) {
+        return Err(err_msg(BAD_SIG));
+    }
+    Ok(obj.body.payload)
 }
 
 #[cfg(test)]
@@ -87,19 +136,34 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_verify_merkle_root() {
-        let key = load_key(ROOT_KEY.as_bytes()).unwrap();
-        let output = verify(ROOT_5360668_PGP.as_bytes(), &key).unwrap();
+    fn test_pgp_verify_merkle_root() {
+        let key = load_pgp_key(ROOT_PGP_KEY.as_bytes()).unwrap();
+        let output = verify_pgp(ROOT_5360668_PGP.as_bytes(), &key).unwrap();
         let output_str = String::from_utf8(output).unwrap();
         assert_eq!(ROOT_5360668_JSON, output_str);
     }
 
     #[test]
-    fn test_wrong_key_fails() {
-        let key = load_key(JACK_KEY.as_bytes()).unwrap();
-        let err = verify(ROOT_5360668_PGP.as_bytes(), &key).unwrap_err();
+    fn test_pgp_missing_key_fails() {
+        let key = load_pgp_key(JACK_PGP_KEY.as_bytes()).unwrap();
+        let err = verify_pgp(ROOT_5360668_PGP.as_bytes(), &key).unwrap_err();
         assert_eq!(err.to_string(), MISSING_KEY);
     }
 
     // TODO: Produce a corrupt signature with a valid CRC and check that.
+
+    #[test]
+    fn test_nacl_verify_merkle_root() {
+        let key = load_nacl_key(ROOT_NACL_KEY).unwrap();
+        let output = verify_kbsig(ROOT_5360668_KBSIG, &key).unwrap();
+        let output_str = String::from_utf8(output).unwrap();
+        assert_eq!(ROOT_5360668_JSON, output_str);
+    }
+
+    #[test]
+    fn test_nacl_verify_wrong_key_fails() {
+        let key = sodiumoxide::crypto::sign::PublicKey([0xff; 32]);
+        let err = verify_kbsig(ROOT_5360668_KBSIG, &key).unwrap_err();
+        assert_eq!(err.to_string(), BAD_SIG);
+    }
 }
